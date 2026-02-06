afbf45fc4e1b0f5f7f2294106f2269f6bdba0ab3
hariraja-07 <hariraja1976@gmail.com>
2026-02-06 22:31:19 +0530

    feat : switch library panel view on button action

--- lib/features/player/widgets/library_panel.dart
+++ lib/features/player/widgets/library_panel.dart
@@ -1,31 +1,103 @@
 import 'package:flutter/material.dart';
 
-class LibraryPanel extends StatelessWidget {
+class LibraryPanel extends StatefulWidget {
   const LibraryPanel({super.key});
+
+  @override
+  State<LibraryPanel> createState() => _LibraryPanelState();
+}
+
+class _LibraryPanelState extends State<LibraryPanel> {
+  bool showFileScreen = false;
+
+  void openFileScreen(){
+    setState(() {
+      showFileScreen = true;
+    });
+  }
+
+  void goBack(){
+    setState(() {
+      showFileScreen = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(12),
-      child: Column(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 20),
-              child: Container(
-                color: Colors.blue,
-              ),
+      child: showFileScreen 
+      ? FileScreen(onBack : goBack) 
+      : BaseScreen(onSelectFolder : openFileScreen),
+    );
+  }
+}
+
+class BaseScreen extends StatelessWidget{
+  final VoidCallback onSelectFolder;
+
+  const BaseScreen({
+    super.key,
+    required this.onSelectFolder,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Container(
+              color: Colors.blue,
             ),
           ),
-          SizedBox(
-            width: double.infinity,
-            child: ElevatedButton(
-              onPressed: (){}, 
-              child: Text("Select Folder")
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: onSelectFolder,
+            child: Text("Select Folder")
+            ),
+        )
+      ],
+    );
+  }
+}
+
+class FileScreen extends StatelessWidget{
+  final VoidCallback onBack;
+
+  const FileScreen({
+    super.key,
+    required this.onBack,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 8),
+          child: Row(
+            children: [
+              IconButton(
+                onPressed: onBack, 
+                icon: Icon(Icons.arrow_back,size: 20,)
               ),
-          )
-        ],
-      ),
+              const SizedBox(width: 8,),
+              const Text(
+                'Selected Folder'
+              )
+
+            ],
+          ),
+        ),
+
+        Expanded(
+          child: Text(""),
+        )
+      ],
     );
   }
 }
