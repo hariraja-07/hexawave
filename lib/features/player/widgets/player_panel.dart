897d320c1f9dcfe65054240245f65829a3866fd7
hariraja-07 <hariraja1976@gmail.com>
2026-02-08 17:27:30 +0530

    feat : add control buttons(play|pause|stop) to the player panel

--- lib/features/player/widgets/player_panel.dart
+++ lib/features/player/widgets/player_panel.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:hexawave/features/player/widgets/control_buttons.dart';
 
 class PlayerPanel extends StatelessWidget{
   const PlayerPanel({super.key});     
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("Player Panel")
-      ],
+    return Center(
+      child: Column(
+        children: [
+          Expanded(child: Text("")),
+          Text("Player Panel"),
+          ControlButtons(),
+          Expanded(child: Text(""))
+        ],
+      ),
     );
   }
 }
